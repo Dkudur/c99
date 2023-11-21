ced497a0873025dfb5240be8b64763dd9f29d419
Dkudur <97417506+Dkudur@users.noreply.github.com>
2023-11-21 01:15:17 -0500

    Add files via upload

---
+++ c99.py
@@ -0,0 +1,73 @@
+# print("*")
+# print("* *")
+# print("* * *")
+# print("* * * *")
+# print("* * * * *")
+
+rows = int(input("Enter the number of rows: "))
+
+# for i in range(0, rows):
+#     for j in range(0, i+1):
+#         print("*", end = " ")
+#     print()
+
+# print("0")
+# print("0 1")
+# print("0 1 2")
+# print("0 1 2 3")
+# print("0 1 2 3 4")
+# print("0 1 2 3 4 5")
+
+# for i in range(0, rows):
+#     for j in range(0, i+1):
+#         print(j, end = " ")
+#     print()
+        
+# print("1")
+# print("1 2")
+# print("1 2 3")
+# print("1 2 3 4")
+
+# for i in range(0, rows):
+#     for j in range(0, i+1):
+#         print(j+1, end = " ")
+#     print()
+
+# for i in range(1, rows + 1):
+#     for j in range(1, i + 1):
+#         print(j, end = " ")
+#     print()
+
+# print("* * * * *") - 0
+# print("* * * *") -> 1
+# print("* * *")
+# print("* *")
+# print("*")
+
+# for i in range(rows, 0, -1):
+#     # print(i)
+#     for j in range(i+1, 1, -1):
+#         print("*", end = " ")
+#     print()
+
+# for i in range(0, rows):
+#     for j in range(rows, i, -1):
+#         print("*", end = " ")
+#     print()
+
+# print("5 4 3 2 1")
+# print("5 4 3 2")
+# print("5 4 3")
+# print("5 4")
+# print("5")
+
+# for i in range(rows, 0, -1):
+#     for j in range(i, 0, -1):
+#         print(j, end = " ")
+#     print()
+
+print("1 2 3 4 5")
+print("1 2 3 4")
+print("1 2 3")
+print("1 2")
+print("1")
